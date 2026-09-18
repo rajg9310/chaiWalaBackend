babd1bf55d3a5eae28cc64237cc611ef0c872020
rajg9310 <rajg931065@gmail.com>
2026-09-19 01:03:20 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,11 +36,11 @@ const registerUser = asyncHandler(async(req,res)=>{
   if(!avatarLocalPath){
     throw new ApiError(408,"Avatar file is required")
   }
-
+  
   const avatar = await uploadOnCloudinary(avatarLocalPath)
   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
   
-
+  
   
   if (!avatar) {
     throw new ApiError(408,"Avatar file is required");
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,12 +48,12 @@ const userSchema = new Schema({
 
 
 
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
